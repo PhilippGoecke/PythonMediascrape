b70b5dfd56691a6dd7f99a360005c9d9121d9ae9
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-17 22:24:32 +0200

    Update dl.py

--- dl.py
+++ dl.py
@@ -244,8 +244,8 @@ def download_media(url, output_dir='downloads', url_whitelist=None, verify_tls=T
 
     # Header
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
-        #'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+        #'Accept': '*/*',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         #'Accept-Encoding': 'gzip, deflate, br',
